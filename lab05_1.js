28816694bff543084d17d3f52e8afcb12945d7f4
tohfatul <tohfatul@users.noreply.github.com>
2022-04-03 23:31:46 -0500

    more updates

--- lab05_1.js
+++ lab05_1.js
@@ -1,6 +1,10 @@
 
 // using fucntion declaration and map
 
+
+// a function using function declaration named sum with one parameter of Array type, the
+// returned result is the sum of all elements which are greater than 20.
+
 console.log(getSum([21,2,30, 5, 23]));
 
 function getSum (param){
--- lab05_2.js
+++ lab05_2.js
@@ -1,10 +1,15 @@
 
 // using function expression and filter
 
+
+// a function using function expression named getNewArray with one parameter of String
+// Array, return a new array which contains all string, length is greater than and equal to 5, and
+// contains letter ‘a’.
+
 let getNewArray = function(param){
     
     let newArr = param.filter(function(elem){
-        return elem.length>=5;
+        return elem.length>=5 && elem.includes('a');
     });
 
     return newArr;
